4ea49e21703dfb363cffd338ecc73ef7c62195d3
John Chae <jongyeonchae@gmail.com>
2021-01-07 23:46:32 +0900

    MVC 패턴을 적용하여 파일 분리
    - M(Model): Data
       V(View): Template
       C(Control): Function
    - route에서 함수를 따로 파일(~routers) 분리

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-// router.js 에서 const userRouter 를 지정해서 export 한 경우
-import { userRouter } from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 
 const app = express();
 
-const handleHome = (req, res) => res.send("Hello from home");
-
-const handleProfile = (req, res) => res.send("Hello, you are on my profile");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -19,12 +18,9 @@ app.use(helmet());
 app.use(morgan("dev"));
 
 
-app.get("/", handleHome);
-
-app.get("/profile", handleProfile);
-
 // get이 아닌 use 메서드를 쓰면, router 파일의 전체 경로를 지정함
-app.use("/user", userRouter);
-
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
