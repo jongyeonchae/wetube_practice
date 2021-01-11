681779285e5fa6b41cfdf8226eb3395a8fb8f3b1
John Chae <jongyeonchae@gmail.com>
2021-01-11 21:21:57 +0900

    Search controller 수정
    - search input 삽입
    - query object를 활용한 검색어 가져오기

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,21 @@
 export const home = (req, res) => res.render('home', { pageTitle: 'Home' });
-export const search = (req, res) => res.render('search', { pageTitle: 'Search' });
+
+export const search = (req, res) => {
+    // ES6 이전 코드로 작성 시, 아래와 동일
+    // const searchingBy = req.query.term;
+    // res.render('search', { pageTitle: 'Search', searchingBy: searchingBy }); 
+    const {
+        query: { term: searchingBy }
+    } = req;    
+    res.render('search', { pageTitle: 'Search', searchingBy });
+};
+
 export const videos = (req, res) => res.render('videos', { pageTitle: 'Videos' });
+
 export const upload = (req, res) => res.render('upload', { pageTitle: 'Upload' });
+
 export const videoDetail = (req, res) => res.render('videoDetail', { pageTitle: 'Video Detail' });
+
 export const editVideo = (req, res) => res.render('editVideo', { pageTitle: 'Edit Video' });
+
 export const deleteVideo = (req, res) => res.render('deleteVideo', { pageTitle: 'Delete Video' });
