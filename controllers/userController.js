7b772d8ab9a6ffefb8297fe99c1a1006a82bc683
John Chae <jongyeonchae@gmail.com>
2021-01-11 20:12:03 +0900

    template 변수 설정
    - 각 pug 파일에 별도의 변수를 설정하는 방법
    - render 함수의 두번째 인자 위치에 삽입된 오브젝트는, 여러 옵션값을 입력하여 해당 템플릿에 정보 추가 가능

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,9 @@
 // arrow function(=>)에는 암묵적으로 return 이 생략되어 있으므로, return을 쓰지 않아도 됨
 // export const join = (req, res) => return res.send('Join'); 과 같음
-export const join = (req, res) => res.render('join');
-export const login = (req, res) => res.render('login');
-export const logout = (req, res) => res.render('logout');
-export const users = (req, res) => res.render('users');
-export const userDetail = (req, res) => res.render('userDetail');
-export const editProfile = (req, res) => res.render('editProfile');
-export const changePassword = (req, res) => res.render('changePassword');
+export const join = (req, res) => res.render('join', { pageTitle: 'Join' });
+export const login = (req, res) => res.render('login', { pageTitle: 'Log In' });
+export const logout = (req, res) => res.render('logout', { ageTitle: 'Log Out' });
+export const users = (req, res) => res.render('users', { pageTitle: 'Users' });
+export const userDetail = (req, res) => res.render('userDetail', { pageTitle: 'User Detail' });
+export const editProfile = (req, res) => res.render('editProfile', { pageTitle: 'Edit Profile' });
+export const changePassword = (req, res) => res.render('changePassword', { pageTitle: 'Change Password' });
