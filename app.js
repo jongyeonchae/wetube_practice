1ad1f0d2a5be7503c4a548fd7f482b92565bd0fd
John Chae <jongyeonchae@gmail.com>
2021-01-11 18:33:58 +0900

    locals 변수 설정
    - res.locals: 전체 pug 파일에 접근할 수 있는 오브젝트
    - middleware 중 가장 상위에서 실행하여 전체 라우팅에 적용
    - res.locals. 뒤에 속성명 작성 후, #{}로 호출

--- app.js
+++ app.js
@@ -3,20 +3,22 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localsMiddleware } from "./middlewares";
+import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
-import routes from "./routes";
 
 const app = express();
 
+app.use(helmet());
 app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(helmet());
 app.use(morgan("dev"));
 
+app.use(localsMiddleware);
 
 // get이 아닌 use 메서드를 쓰면, router 파일의 전체 경로를 지정함
 app.use(routes.home, globalRouter);
