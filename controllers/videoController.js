34b8a0884d9f7d10e950109fdc3b43a11da0013b
John Chae <jongyeonchae@gmail.com>
2021-01-18 06:31:22 +0900

    video 업로드 및 생성
    multer
    - 등록된 파일의 url 접근 가능한 middleware
    - video url(path) 확인 후, 해당 video id를 활용하여 상세 페이지로 이동

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -21,12 +21,24 @@ export const search = (req, res) => {
 export const getUpload = (req, res) => 
     res.render('upload', { pageTitle: 'Upload' });
 
-export const postUpload = (req, res) => {
+// file url 확인 
+// export const postUpload = (req, res) => {    
+//     const { body, file } = req;
+//     console.log(body, file);
+//     res.render("upload", { pageTitle: "Upload" });
+// };
+export const postUpload = async (req, res) => {
     const {
-        body: { file, title, description } 
+        body: { title, description },
+        file: { path }
     } = req;
-    // To Do: Upload and save Video
-    res.redirect(routes.videoDetail(324245));
+    const newVideo = await Video.create({
+        fileUrl: path,
+        title,
+        description
+    });
+    console.log(newVideo);    
+    res.redirect(routes.videoDetail(newVideo.id));
 };
 
 export const videoDetail = (req, res) => res.render('videoDetail', { pageTitle: 'Video Detail' });
