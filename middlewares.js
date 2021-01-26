b53dd900c7dc4860ad9b5a6af52d82ae748adb59
John Chae <jongyeonchae@gmail.com>
2021-01-26 21:52:35 +0900

    [webpack] front-end 작성
    - pug, sass(scss) 를 활용
    - sass 작성 시, selector를 father 요소 안에 위치 가능

--- middlewares.js
+++ middlewares.js
@@ -4,15 +4,15 @@ import routes from "./routes";
 // dest: /uploads/videos/ 로 표기할 시, 프로젝트 파일(WETUBE) 내 directory 로 오해할 수 있음 (서버가 아닌 하드웨어에 업로드)
 const multerVideo = multer({ dest: "uploads/videos/" });
 
-export const localsMiddleware = (req, res, next)  => {
-    res.locals.siteName = "WeTube";
-    res.locals.routes = routes;
-    // login 된 상태를 설정하기 위해 임의의 변수(user) 생성
-    res.locals.user = {
-        isAuthenticated: true,
-        id: 1
-    };
-    next();
-}
+export const localsMiddleware = (req, res, next) => {
+  res.locals.siteName = "WeTube";
+  res.locals.routes = routes;
+  // login 된 상태를 설정하기 위해 임의의 변수(user) 생성
+  res.locals.user = {
+    isAuthenticated: false,
+    id: 1,
+  };
+  next();
+};
 
 export const uploadVideo = multerVideo.single("videoFile");
