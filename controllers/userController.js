2c7c013450004c1ac9a03b871d63fdc9d26097b3
John Chae <jongyeonchae@gmail.com>
2021-02-10 22:15:18 +0900

    changePassword 페이지 수정
    - passport local mongoose 를 활용하여 password 설정 진행(password는 암호화된 상태로 입력되는데, changePassword()를 통해 암호화된 password의 확인 및 수정 가능)
    - 새로운 password 확인 시 status code 200을 명시해야 크롬 팝업(password 저장)이 안나타남

--- controllers/userController.js
+++ controllers/userController.js
@@ -119,25 +119,25 @@ export const postEditProfile = async (req, res) => {
     });
     res.redirect(routes.me);
   } catch (error) {
-    res.render("editProfile", { pageTitle: "Edit Profile" });
+    res.redirect(routes.editProfile);
   }
 };
 
-// export const postEditProfile = async (req, res) => {
-//   const {
-//     body: { name, email },
-//     file,
-//   } = req;
-//   try {
-//     await User.findByIdAndUpdate(req.user.id, {
-//       name,
-//       email,
-//       avatarUrl: file ? file.path : req.user.avatarUrl,
-//     });
-//     res.redirect(routes.me);
-//   } catch (error) {
-//     res.render("editProfile", { pageTitle: "Edit Profile" });
-//   }
-// };
+export const getChangePassword = (req, res) => res.render("changePassword", { pageTitle: "Change Password" });
 
-export const changePassword = (req, res) => res.render("changePassword", { pageTitle: "Change Password" });
+export const postChangePassword = async (req, res) => {
+  const {
+    body: { oldPassword, newPassword, newPassword1 },
+  } = req;
+  try {
+    if (newPassword !== newPassword1) {
+      res.status(400);
+      res.redirect(`/users/${routes.changePassword}`);
+      return;
+    }
+    await req.user.changePassword(oldPassword, newPassword);
+    res.redirect(routes.me);
+  } catch (error) {
+    res.redirect(`/users/${routes.changePassword}`);
+  }
+};
