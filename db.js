62c01da36eee219e327e3c987b086b4d638d0c65
John Chae <jongyeonchae@gmail.com>
2021-03-05 11:27:13 +0900

    [MongoDB Atlas] DB Migration
    - Mongo DB에서 Mongo DB Atlas로 전환
    
    Mongo DB Atlas 설정
    - Cluster 생성 (Cluster0)
    - Cluster0를 application에 connect (MongoDB user 생성, connection string 복붙)

--- db.js
+++ db.js
@@ -3,22 +3,17 @@ import dotenv from "dotenv";
 // dotenv.config(): .env 파일 import 및 파일 내 변수 접근 가능(process.env.KEY)
 dotenv.config();
 
-
 // terminal 에서 mongo 를 입력하여 mongo url 확인
 // useNewUrlParser, useFindMyModify 설정은 mongodb에서 요청사항이므로 기본으로 입력
-mongoose.connect(
-    process.env.MONGO_URL, 
-    {
-    useNewUrlParser: true,
-    useFindAndModify: false
-    }
-);
+mongoose.connect(process.env.MONGO_URL_PROD, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
 
 const db = mongoose.connection;
 
 const handleOpen = () => console.log("✅ Connected to DB");
 const handleError = (error) => console.log(`❌ Error on DB Connection:${error}`);
-
 
 db.once("open", handleOpen);
 db.on("error", handleError);
